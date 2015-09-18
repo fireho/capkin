f9777a583576c32749468bcf44c599cc562d6d42
Marcos Piccinini <x@nofxx.com>
2015-09-18 01:48:25 -0300

    refactor

--- lib/capkin/cli.rb
+++ lib/capkin/cli.rb
@@ -16,62 +16,14 @@ module Capkin
 
       def read_file
         @config = YAML.load_file('Capkin')
-        puts Paint["Config file OK! APP: '#{@config['app']}'", :green]
-      end
-
-      # To be refactored in robot.rb
-      def upload_apk
-        # Get package
-        app = @config['name']
-        source = @config['build'] + app + '.apk'
-
-        puts Paint["Publishing new APK: ./#{source} to 'Play'", :green]
-
-        # create a publisher object
-        pub = Google::Apis::AndroidpublisherV2::AndroidPublisherService.new
-
-        # Get authorization!
-        scopes = ['https://www.googleapis.com/auth/androidpublisher']
-        auth = Google::Auth.get_application_default(scopes)
-
-        # Oauth2
-        # grant_type=authorization_token|refresh_token
-        # code=
-        # client_id=<the client ID token created in the APIs Console>
-        # client_secret=<the client secret corresponding to the client ID>
-        # refresh_token=<the refresh token from the previous step>
-        # start a new edit
-        # headers = {
-        #   'access_token' => ENV['CAPKIN_CLIENT']
-        # }
-        # auth.apply(headers)
-        pub.authorization = auth
-
-        # Create new edition
-        edit = pub.insert_edit(app)
-
-        # upload the APK
-        apk = pub.upload_apk(app, edit.id, upload_source: source)
-
-        # Update the alpha track to point to this APK
-        track = pub.get_track(app, edit_id, 'alpha')
-
-        # You need to use a track object to change this
-        track.update!(version_codes: [apk.version_code])
-
-        # Save the modified track object
-        pub.update_track(app, edit_id, 'production', track)
-
-        # Commit the edit
-        pub.commit_edit(app, edit_id)
-
-        puts 'DONE'
+        msg = "Config file OK! '#{@config['app']}' -> #{@config['name']}"
+        puts Paint[msg, :green]
       end
 
       def work!(params)
         check_capkin_file
         read_file
-        upload_apk
+        Capkin::Robot.new(@config, params).upload_apk!
       end
     end
   end
--- lib/capkin/robot.rb
+++ lib/capkin/robot.rb
@@ -1,4 +1,60 @@
 module Capkin
+  # Oauth2
+  # grant_type=authorization_token|refresh_token
+  # code=
+  # client_id=<the client ID token created in the APIs Console>
+  # client_secret=<the client secret corresponding to the client ID>
+  # refresh_token=<the refresh token from the previous step>
+  # start a new edit
+  # headers = {
+  #   'access_token' => ENV['CAPKIN_CLIENT']
+  # }
+  # auth.apply(headers)
   class Robot
+    SCOPE = ['https://www.googleapis.com/auth/androidpublisher']
+    attr_reader :apk, :app, :pub, :pkg, :stage, :track, :source
+
+    def initialize(config, stage = 'alpha')
+      @app = config['app']
+      @pkg = config['name']
+      @source ||= File.join(config['build'], "#{@app}.apk")
+      @stage = stage
+
+      # Get authorization!
+      @auth = Google::Auth.get_application_default(SCOPE)
+      # create a publisher object
+      @pub = Google::Apis::AndroidpublisherV2::AndroidPublisherService.new
+      @pub.authorization = @auth
+    end
+
+    # Create new edition
+    def edit
+      @edit ||= pub.insert_edit(pkg)
+    end
+
+    def upload_apk!
+      puts Paint["Publishing new APK: ./#{source} to 'Play'", :blue]
+      # upload the APK
+      @apk = pub.upload_apk(pkg, edit.id, upload_source: source)
+      puts Paint["APK uploaded #{apk.version_code}", :yellow]
+      track!
+    end
+
+    def track!
+      # Update the alpha track to point to this APK
+      @track = pub.get_track(pkg, edit.id, stage)
+
+      # You need to use a track object to change this
+      track.update!(version_codes: [apk.version_code])
+      commit!
+    end
+
+    def commit!
+      # Save the modified track object
+      pub.update_track(pkg, edit.id, stage, track)
+
+      # Commit the edit
+      pub.commit_edit(pkg, edit.id)
+    end
   end
 end
